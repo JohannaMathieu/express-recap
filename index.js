15b6e05377d783121eefc840c35cf8583f72e14e
JohannaMathieu <johannamathieu@yahoo.de>
2021-05-25 12:27:40 +0200

    add cors

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const connection = require('./db-config');
 const express = require('express');
+var cors = require('cors')
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
+
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
